f5b3295274bf495ab2388938cf9e3781bf65d7a0
sergious234 <sergious234@gmail.com>
2022-05-07 15:26:31 +0200

    Formatteo correcto

--- practica6/main.c
+++ practica6/main.c
@@ -1,26 +1,23 @@
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/msg.h>
-#include <errno.h>
 #include <time.h>
 #include <unistd.h>
 
-struct Numero
-{
+struct Numero {
     long Id_Men; // 1 -> Positivo, 2 -> Negativo
     int Numero;
 };
 
-void ProcesoA(int Clave)
-{
+void ProcesoA(int Clave) {
     int Id_Cola;
     int num_aletatorio;
 
     Id_Cola = msgget(Clave, 0600 | IPC_CREAT);
 
-    if(Id_Cola == 0)
-    {
+    if (Id_Cola == 0) {
         printf("Error al abrir la cola en A");
     }
 
@@ -29,8 +26,7 @@ void ProcesoA(int Clave)
     int sleep_time;
     srand(time(NULL));
 
-    while(contador != 10)
-    {
+    while (contador != 10) {
         sleep_time = rand() % 2 + 1;
 
         sleep(sleep_time);
@@ -40,92 +36,76 @@ void ProcesoA(int Clave)
         numero.Id_Men = (num_aletatorio < 0 ? 2 : 1);
         numero.Numero = num_aletatorio;
 
-        msgsnd(Id_Cola, (struct msgbuf *)&numero, sizeof(numero)-sizeof(long), 0);
+        msgsnd(Id_Cola, (struct msgbuf *)&numero, sizeof(numero) - sizeof(long),
+               0);
         contador++;
     }
     msgctl(Id_Cola, IPC_RMID, (struct msqid_ds *)NULL);
 }
 
-void ProcesoB(int Clave)
-{
+void ProcesoB(int Clave) {
     int Id_Cola;
 
     Id_Cola = msgget(Clave, 0600 | IPC_CREAT);
 
-    if(Id_Cola == 0)
-    {
+    if (Id_Cola == 0) {
         printf("Error al abrir la cola en B");
     }
 
     struct Numero numero;
     int leido;
-    
-    do
-    {
 
-    }while(leido != 0);
+    do {
 
-} 
+    } while (leido != 0);
+}
 
-void ProcesoC(int Clave)
-{
+void ProcesoC(int Clave) {
     int Id_Cola;
 
     Id_Cola = msgget(Clave, 0600 | IPC_CREAT);
 
-
-    if(Id_Cola == 0)
-    {
+    if (Id_Cola == 0) {
         printf("Error al abrir la cola en C");
     }
 
     struct Numero numero;
 }
 
-
-int main()
-{
+int main() {
     int forks;
 
     key_t Clave;
     int Id_Cola;
 
     Clave = ftok("Makefile", 14);
-    if(Clave == (key_t)-1)
-    {
+    if (Clave == (key_t)-1) {
         printf("Error al crear la clave");
         exit(-1);
     }
 
-    Id_Cola = msgget(Clave, 0600); //Asegurarse que la cola no está ya creada
+    Id_Cola = msgget(Clave, 0600); // Asegurarse que la cola no está ya creada
 
-    if(Id_Cola != -1)
-    {
-        msgctl (Id_Cola, IPC_RMID, (struct msqid_ds *)NULL);
+    if (Id_Cola != -1) {
+        msgctl(Id_Cola, IPC_RMID, (struct msqid_ds *)NULL);
     }
 
     forks = fork();
 
-    if(forks == 0)
-    {
+    if (forks == 0) {
         ProcesoB(Clave);
         exit(0);
-    }
-    else if(forks == -1)
-    {
+    } else if (forks == -1) {
         printf("Error al forkear B");
         exit(-1);
     }
 
     forks = fork();
 
-    if(forks == 0)
-    {
+    if (forks == 0) {
         ProcesoC(Clave);
         exit(0);
-    }
-    else if(forks == -1)
-    {
+    } else if (forks == -1) {
         printf("Error al forkear C");
         exit(-1);
     }
